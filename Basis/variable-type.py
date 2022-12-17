6f7a5cec3b38e305e19c3c2d79c00c914f5f0748
andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-16 20:52:47 -0800

    Update variable-type.py

--- Basis/variable-type.py
+++ Basis/variable-type.py
@@ -1,3 +1,9 @@
+#Affectation de valeurs à des variables:
 x = 10
 y = "hello"
 z = [1, 2, 3]
+
+#Utilisation de variables dans une instruction print 
+
+name = "John"
+print(f"Bonjour, {name}!")  # Affiche "Bonjour, John!"
