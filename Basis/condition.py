0f74305595a7efb2abde2a96e7716f22af71b0ed
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-19 03:36:15 -0800

    Update condition.py

--- Basis/condition.py
+++ Basis/condition.py
@@ -1,1 +1,18 @@
 # instructions conditionelles 
+# Si l'expression est vraie, le code dans le bloc if est exécuté
+if expression:
+    # code du bloc if
+
+# Si l'expression est vraie, le code dans le bloc if est exécuté, sinon le code dans le bloc else est exécuté
+if expression:
+    # code du bloc if
+else:
+    # code du bloc else
+
+# Si l'expression est vraie, le code dans le premier bloc if est exécuté, sinon, si l'expression dans le second bloc elif est vraie, le code dans ce bloc est exécuté, sinon le code dans le bloc else est exécuté
+if expression1:
+    # code du premier bloc if
+elif expression2:
+    # code du bloc elif
+else:
+    # code du bloc else
