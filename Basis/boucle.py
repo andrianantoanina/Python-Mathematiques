78cfcfa30d10e6d8679d98a17a19e01317abf08d
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-21 02:28:46 -0800

    Create boucle.py

---
+++ Basis/boucle.py
@@ -0,0 +1,3 @@
+# loop in Python is a way to repeat a block of code a certain number of times, or until a certain condition is met. There are two types of loops in Python: for loops and while loops.
+for i in range(10):
+    print(i)
