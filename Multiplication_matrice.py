65a099bf8bec8848553bf2f23da72d129ac80d50
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-23 06:08:31 +0300

    Update Multiplication_matrice.py

--- Multiplication_matrice.py
+++ Multiplication_matrice.py
@@ -1,1 +1,11 @@
 
+import numpy as np
+
+# Définir les matrices A et B
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+# Effectuer le produit de matrices
+C = np.dot(A, B)
+
+print(C)
