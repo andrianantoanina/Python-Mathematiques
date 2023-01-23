5db42eeebc963cb68986daf386ebfb86d8938f62
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2023-01-22 20:45:10 -0800

    Update fonction.py

--- Basis/fonction.py
+++ Basis/fonction.py
@@ -3,6 +3,6 @@ def ma_fonction(argument1, argument2):
   resultat = argument1 + argument2
   return resultat
 
-# Appeler la fonction avec tous les arguments optionnels
+# Appeler la fonction avec tous les argumentss optionnels
 resultat = ma_fonction(10, 20, 30, 40)
 print(resultat)  # Affiche 100
