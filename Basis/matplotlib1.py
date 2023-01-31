3d9d5acaad66d7d2aefc065d27b24affa7a65e73
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2023-01-30 22:28:40 -0800

    Update matplotlib1.py

--- Basis/matplotlib1.py
+++ Basis/matplotlib1.py
@@ -1,1 +1,15 @@
 
+# importing matplotlib module 
+from matplotlib import pyplot as plt
+  
+# x-axis values
+x = [5, 2, 9, 4, 7]
+  
+# Y-axis values
+y = [10, 5, 8, 4, 2]
+  
+# Function to plot
+plt.plot(x,y)
+  
+# function to show the plot
+plt.show()
